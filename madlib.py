4cbb08ba97e6cf71ace7573977f191b8f7b01ef3
oliver122337 <oliver122337@mcpsva.org>
2026-09-02 10:32:56 -0400

    code so far (9/2/26) B2

--- madlib.py
+++ madlib.py
@@ -7,4 +7,27 @@ Instructor: Burgess
 """
 
 
+q1=input("Adjective")
+q2=input("noun")
+q3=input("adverb")
+q4=input("noun")
+q5=input("verb")
+q6=input("noun")
+q7=input("plural noun")
+q8=input("verb")
+q9=input("adjective")
+q10=input("adverb")
+q11=input("adjective")
+q12=input("verb")
+q13=input("adjective")
+q14=input("noun")
+q15=input("verb")
+q16=input("adjective")
+q17=input("noun")
+q18=input("verb")
+q19=input("plural noun")
+q20=input("number")
 
+print("To build a",q1,"computer, first insert the",q2,"into the motherboard",q3,". \nDo not forget the",q4," paste, or your processor will",q5," like a",q6,". ")
+print(" Next, slide the RAM into its",q7," until they",q8,". Mount the",q9,"power supply and plug in the cables",q10,". ")
+print("If your graphics card is too",q11,", it might not",q12,"inside the case. Finally, push the",q13,"power button. ")
