0625332955c32c49f1b1af737738e03468fef768
oliver122337 <oliver122337@mcpsva.org>
2026-09-10 10:05:09 -0400

    code so far (9/2/26) B2

--- calculator.py
+++ calculator.py
@@ -7,6 +7,27 @@ Instructor: Burgess
 """
 
 
-a1=input("question here:")
+n1=input("first number:")
+n2=input("second number:")
+sign=input("sign:")
+
+n1=int(n1)
+n2=int(n2)
+
+if sign=="+":
+    print(n1,"+",n2,"=",n1+n2)
+elif sign=="-":
+
+        print(n1,"-",n2,"=",n1-n2)
+
+elif sign=="*":
+        print(n1,"*",n2,"=",n1*n2)
+
+elif sign=="/":
+        print(n1,"/",n2,"=",n1/n2)
+
+
+
+
 
 
